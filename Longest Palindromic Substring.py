4c85dabdc0ebfb7713d5844509a2d0d576a5d846
hecter_wu <hecter_wu@transcend.com.tw>
2017-08-23 14:59:36 +0800

    pass testCase: testCase1 and testCase2 by using recursive

--- Longest Palindromic Substring.py
+++ Longest Palindromic Substring.py
@@ -5,11 +5,31 @@ class unitest(unittest.TestCase):
         s = ""
         ans = ""
         self.assertEqual(Solution().longestPalindrome(s),ans);
+    def testCase1(self):
+        s = "cbbd"
+        ans = "bb"
+        self.assertEqual(Solution().longestPalindrome(s),ans);
+    def testCase2(self):
+        s = "babad"
+        ans = "bab"
+        self.assertEqual(Solution().longestPalindrome(s),ans);
 
 class Solution():
     def longestPalindrome(self, s):
+        def IsPalindrome(s):
+            if s == s[::-1]:
+                return True
+            else:
+                return False
         if s == "":
             return s
+        for i in range(1,len(s)+1):
+            if IsPalindrome(s[:i]):
+                Ans = s[:i]
+        ParentAns = Solution().longestPalindrome(s[1:i])
+        if len(ParentAns) > len(Ans):
+            return ParentAns
+        return Ans
 
 if __name__ == '__main__':
     unittest.main()
